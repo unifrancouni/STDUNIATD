2c354ba55856d53c36e7cc6ec86afa976fdce1f0
Franco <unifrancouni@gmail.com>
2015-03-02 23:53:35 -0600

    Combinacion con bootstrap, htacces inseguro

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -19,7 +19,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		//$data['contenido']='home/index'; 
+        $this->load->view('invitado/a');
 	}
 }
 
--- application/views/desarrollador/index.php
+++ application/views/desarrollador/index.php
@@ -66,24 +66,27 @@
 </head>
 <body>
 
-<?= base_url()?>
-
 <div id="container">
 	<h1>Welcome to CodeIgniter!</h1>
 
+	<!--
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
 		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+		<code>application/views/welcome_message.php
+
+				Entonces no  es cosdigo.
+
+		</code>
 
 		<p>The corresponding controller for this page is found at:</p>
 		<code>application/controllers/welcome.php</code>
 
 		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
+	</div> -->
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+	<p class="footer">Página cargada en: <strong>{elapsed_time}</strong> seconds</p>
 </div>
 
 </body>
