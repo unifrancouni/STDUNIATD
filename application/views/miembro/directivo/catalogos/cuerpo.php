eb5f5a61a54d9e003824dbdbdf0378e950723c72
Franco <unifrancouni@gmail.com>
2015-05-05 01:04:12 -0600

    Catalogos, valorcatalogo sin fx

--- application/views/miembro/directivo/catalogos/cuerpo.php
+++ application/views/miembro/directivo/catalogos/cuerpo.php
@@ -1,155 +1,1 @@
-<!-- ***************** -->
-<!-- Inicio del Cuerpo -->
-<!-- ***************** -->
-
-<div class="content-wrapper">
-
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <h1>
-            Catalogos
-            <small>Administrador</small>
-        </h1>
-        <ol class="breadcrumb">
-            <li><a href="<?php echo base_url().'dashboard'; ?>"><i class="fa fa-dashboard"></i> Panel de control</a></li>
-            <!-- <li class="active">Dashboard</li> -->
-        </ol>
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-        <div class="row">
-            <div class="col-md-7">
-                <div class="box box-primary">
-                    <div class="box-header">
-                        <h3 class="box-title">Catálogos</h3>
-                    </div><!-- /.box-header -->
-                    <div class="box-body">
-                        <table id="example1" class="table table-bordered table-hover">
-                            <thead>
-                            <tr>
-                                <th>A</th>
-                                <th>B</th>
-                                <th>C</th>
-                                <th>D</th>
-                                <th>E</th>
-                            </tr>
-                            </thead>
-                            <tfoot>
-                                <th></th>
-                                <th></th>
-                                <th></th>
-                                <th></th>
-                                <th></th>
-                            </tfoot>
-                            <tbody>
-                                <tr>
-                                    <td>Franco</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                </tr>
-                                <tr>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                </tr>
-                                <tr>
-                                    <td>Cachos</td>
-                                    <td>Franco</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                </tr>
-                                <tr>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                </tr>
-                                <tr>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                </tr>
-                                <tr>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                </tr>
-                                <tr>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                </tr>
-                                <tr>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                </tr>
-                                <tr>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                </tr>
-                                <tr>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                </tr>
-                                <tr>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                </tr>
-                                <tr>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                    <td>Cachos</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div><!-- /.box-body -->
-                </div><!-- /.box -->
-            </div><!-- /.col -->
-        </div><!-- /.row -->
-        <div class="row">
-            <div class="col-md-9">
-                <div class="box box-primary">
-                    <div class="box-header">
-                        <h3 class="box-title">Catálogos</h3>
-                    </div><!-- /.box-header -->
-                    <div class="box-body">
-                        Esto es una prueba
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-</div> <!-- div Wraper -->
-
-
-<!-- ***************** -->
-<!-- Fin del Cuerpo -->
-<!-- ***************** -->
-
+<!-- ***************** --><!-- Inicio del Cuerpo --><!-- ***************** --><div class="content-wrapper">    <!-- Content Header (Page header) -->    <section class="content-header">        <h1>            Catalogos            <small>Administrador</small>        </h1>        <ol class="breadcrumb">            <li><a href="<?php echo base_url().'dashboard'; ?>"><i class="fa fa-dashboard"></i> Panel de control</a></li>            <!-- <li class="active">Dashboard</li> -->        </ol>    </section>    <!-- Main content -->    <section class="content">        <div class="row">            <div class="col-xs-6 col-md-6">                <div class="box box-primary">                    <div class="box-header">                        <h3 class="box-title">Catálogos</h3>                    </div><!-- /.box-header -->                    <div class="box-body">                        <table id="c" class="table table-bordered table-hover">                            <thead>                            <tr>                                <th>Código</th>                                <th>Catálogo</th>                                <th>Activo</th>                                <th>Acciones</th>                            </tr>                            </thead>                            <tbody>                            <?php foreach($Catalogos as $fila):?>                                <tr>                                    <th><?= $fila->sCodigoInterno ?></td>                                    <th><?= $fila->sDescripcion ?></td>                                    <th><?= $fila->nActivo ?></td>                                    <th><?= $fila->nStbCatalogoID ?></td>                                </tr>                            <?php endforeach; ?>                            </tbody>                        </table>                    </div><!-- /.box-body -->                </div><!-- /.box -->            </div><!-- /.col -->            <div class="col-xs-6 col-md-6">                <div class="box box-primary">                    <div class="box-header">                        <h3 class="box-title">Valores de Catálogos</h3>                    </div><!-- /.box-header -->                    <div class="box-body">                        <table id="vc" class="table table-bordered table-hover">                            <thead>                            <tr>                                <th>Código</th>                                <th>Valor</th>                                <th>Activo</th>                                <th>Acciones</th>                            </tr>                            </thead>                            <tbody>                            <?php foreach($Catalogos as $fila):?>                                <tr>                                    <th><?= $fila->sCodigoInterno ?></td>                                    <th><?= $fila->sDescripcion ?></td>                                    <th><?= $fila->nActivo ?></td>                                    <th><?= $fila->nStbCatalogoID ?></td>                                </tr>                            <?php endforeach; ?>                            </tbody>                        </table>                    </div><!-- /.box-body -->                </div><!-- /.box -->            </div><!-- /.col -->        </div><!-- /.row -->        <div class="row">            <div class="col-md-9">                <div class="box box-primary">                    <div class="box-header">                        <h3 class="box-title">Catálogos</h3>                    </div><!-- /.box-header -->                    <div class="box-body">                        Esto es una prueba                    </div>                </div>            </div>        </div>    </section></div> <!-- div Wraper --><!-- ***************** --><!-- Fin del Cuerpo --><!-- ***************** -->
